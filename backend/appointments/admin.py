ba7083e4eb35a782350640beb1bb348665a1db7e
Victor Manuel Valadez <56981285+victorvaladez1@users.noreply.github.com>
2026-01-21 20:13:08 -0600

    Define initial DB schema and apply migrations to Supabase

--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Barber, Service, Customer, Appointment, Notification
 
-# Register your models here.
+admin.site.register(Barber)
+admin.site.register(Service)
+admin.site.register(Customer)
+admin.site.register(Appointment)
+admin.site.register(Notification)
